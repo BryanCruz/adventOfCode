4649130a2c78f805b53ebd2d633524e5cc0fa0c8
BryanCruz <bryan.bialokur@gmail.com>
2020-12-14 08:00:01 -0300

    feat: add day 14 part 2

--- day-14/main.ts
+++ day-14/main.ts
@@ -68,9 +68,63 @@ const main = (vs: string[]) => {
   return sum;
 };
 
-const main2 = (vs: string[]) => {};
+const applyMask2 = (mask: string, value: bigint): bigint[] => {
+  const valueString = value.toString(2).split("");
+  const prefix: string[] = new Array(mask.length - valueString.length).fill(
+    "0"
+  );
+  const valuesString = [prefix.concat(valueString)];
 
-const [vt, v] = ["inputT.txt", "input.txt"].map((fileName) =>
+  for (let i = 0; i < mask.length; i++) {
+    if (mask[i] === "0" || mask[i] === "1") {
+      valuesString.forEach((valueString) => {
+        valueString[i] = mask[i] === "0" ? valueString[i] : "1";
+      });
+      continue;
+    }
+
+    const newValuesString = valuesString.map((vs) => vs.map((v) => v));
+    newValuesString.forEach((valueString) => {
+      valueString[i] = valueString[i] === "0" ? "1" : "0";
+    });
+
+    valuesString.push(...newValuesString);
+  }
+
+  return valuesString.map((valueString) =>
+    BigInt("0b".concat(valueString.join("")))
+  );
+};
+
+const main2 = (vs: string[]) => {
+  const is = getInstructions(vs);
+
+  let currentMask = null;
+  let memory = new Map<bigint, bigint>();
+  for (const i of is) {
+    if (i.type === "mask") {
+      currentMask = i.mask!;
+      continue;
+    }
+
+    const memoryAddresses = applyMask2(currentMask, i.memoryAddress!);
+    memoryAddresses.forEach((memoryAddress) => {
+      memory.set(memoryAddress, i.value!);
+    });
+  }
+
+  let sum = BigInt(0);
+  memory.forEach((v) => {
+    sum += v;
+  });
+  return sum;
+};
+
+const [vt, vt2, v] = [
+  "inputT.txt",
+  "inputT2.txt",
+  "input.txt",
+].map((fileName) =>
   readFileSync(`${__dirname}/${fileName}`).toString().split("\n")
 );
 
@@ -79,5 +133,5 @@ console.log("test", main(vt));
 console.log("final", main(v));
 console.log("==========");
 console.log("Second Half");
-console.log("test", main2(vt));
+console.log("test", main2(vt2));
 console.log("final", main2(v));
