758f148efd73d0fe748ce8bb367c5f6867332e21
BryanCruz <bryan.bialokur@gmail.com>
2020-12-25 00:25:55 -0300

    feat: add day 22 part 2

--- day-22/main.ts
+++ day-22/main.ts
@@ -31,7 +31,47 @@ const main = (vs: string[]) => {
   return winningHand.reduce((s, v, i) => s + v * (winningHand.length - i), 0);
 };
 
-const main2 = (vs: string[]) => {};
+const recursiveCombat = (hands: [handType, handType]): [number, handType] => {
+  const previousRounds = new Set<string>();
+
+  while (hands.every((hand) => hand.length > 0)) {
+    const handsHash = hands.map((hand) => hand.join(",")).join("-");
+    if (previousRounds.has(handsHash)) {
+      return [0, []];
+    }
+    previousRounds.add(handsHash);
+
+    const cards = hands.map((hand) => hand.shift());
+    let winner = -1;
+    if (cards.every((card, i) => hands[i].length >= card)) {
+      const [recursiveWinner, _] = recursiveCombat(
+        hands.map((hand, i) => hand.slice(0, cards[i])) as [handType, handType]
+      );
+
+      winner = recursiveWinner;
+    } else {
+      winner = cards[0] > cards[1] ? 0 : 1;
+    }
+
+    hands[winner].push(...[cards[winner], cards[1 - winner]]);
+  }
+
+  for (const i of [0, 1]) {
+    if (hands[i].length > 0) {
+      return [i, hands[i]];
+    }
+  }
+};
+
+const main2 = (vs: string[]) => {
+  const hands = parseInput(vs);
+
+  const [_, winningHand] = recursiveCombat(
+    hands.map((hand) => [...hand]) as [handType, handType]
+  );
+
+  return winningHand.reduce((s, v, i) => s + v * (winningHand.length - i), 0);
+};
 
 const [vt, v] = ["inputT.txt", "input.txt"].map((fileName) =>
   readFileSync(`${__dirname}/${fileName}`).toString().split("\n")
