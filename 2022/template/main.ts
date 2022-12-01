88c8ea83c9e90f46b0ca7b5e1fd898569fc9a28a
BryanCruz <bryan.bialokur@gmail.com>
2022-12-01 02:46:35 -0300

    add bootstrap template

---
+++ 2022/template/main.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from "fs";
+
+export {};
+
+const main = (vs: string[]) => {};
+
+const main2 = (vs: string[]) => {};
+
+const [vt, v] = ["inputT.txt", "input.txt"].map((fileName) =>
+  readFileSync(`${__dirname}/${fileName}`).toString().split(/\r?\n/)
+);
+
+console.log("First Half");
+console.log("test", main(vt));
+console.log("final", main(v));
+console.log("==========");
+console.log("Second Half");
+console.log("test", main2(vt));
+console.log("final", main2(v));
